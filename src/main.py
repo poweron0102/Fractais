b9265ec16495e1ef75805298334a0064b86b4496
poweron0102 <nathanpinheiro99@gmail.com>
2025-05-24 20:48:25 -0300

    Muito top

--- src/main.py
+++ src/main.py
@@ -8,12 +8,11 @@ from Features.Edge import *
 from Features.Color import replace
 
 pg.init()
+screen = pg.display.set_mode((800, 600))
 
 
-if __name__ == "__main__":
+def main1():
     print("Hello, World!")
-
-    screen = pg.display.set_mode((800, 600))
 
     img_1 = LoadImage("imgs/frieren_1.png")
     img_2 = LoadImage("imgs/frieren_2.png")
@@ -24,3 +23,26 @@ if __name__ == "__main__":
     replaced_img = replace(fragmentos_1, fragmentos_2, yuv=True)
 
     SaveImage(replaced_img, "imgs/replaced.png")
+
+
+def main2():
+    num_imgs = len(os.listdir("imgs/frames_bad_apple/"))
+
+    future: int = 100
+
+    for i in range(num_imgs):
+        img_1 = LoadImage(f"imgs/frames_bad_apple/{i+1:03d}.jpg")
+        img_2 = LoadImage(f"imgs/frames_bad_apple/{((i + future) % num_imgs) + 1:03d}.jpg")
+
+        fragmentos_1 = get_fragmentos(img_1, 16)
+        fragmentos_2 = get_fragmentos(img_2, 16)
+
+        replaced_img = replace(fragmentos_1, fragmentos_2, yuv=True)
+
+        screen.fill((0, 0, 0))
+        screen.blit(pg.surfarray.make_surface(replaced_img), (0, 0))
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    main2()
